ba0293a1ac9203555db53ef2ebb17e77bef8ecdb
drago467 <tdt.trandinhtuan467@gmail.com>
2024-03-27 21:52:13 +0700

    Deleting Related item & Deleting Cart Products

--- app.js
+++ app.js
@@ -43,8 +43,8 @@ Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
  
 sequelize
-    .sync({ force: true })
-    // .sync()
+    // .sync({ force: true })
+    .sync()
     .then(result => {
         return User.findByPk(1);
         // console.log(result);
@@ -56,8 +56,8 @@ sequelize
         return user;
     })
     .then(user => {
-        console.log(user);
-        return user.createCart();
+        // console.log(user);
+        return user.getCart();
     })
     .then(cart => {
         app.listen(3000);
